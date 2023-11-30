9c8938dc031eff4e03d735050c49ee3b47453a2b
GoutamAravindPatro-gap <145236149+GoutamAravindPatro-gap@users.noreply.github.com>
2023-11-30 18:53:41 -0500

    Pushing the Rolespermission changes

--- 6_RolesPermissions.sql
+++ 6_RolesPermissions.sql
@@ -3,40 +3,6 @@ set serveroutput on
 DECLARE
     ncount NUMBER;
 BEGIN
-    -- Check if PRODUCTION_COMPANY_ROLE exists before attempting to drop
-    SELECT
-        COUNT(1)
-    INTO ncount
-    FROM
-        dba_roles
-    WHERE
-        role = 'PRODUCTION_COMPANY_ROLE';
-
-    IF ( ncount > 0 ) THEN
-        -- Drop PRODUCTION_COMPANY_ROLE if it exists
-        EXECUTE IMMEDIATE 'DROP ROLE PRODUCTION_COMPANY_ROLE';
-        dbms_output.put_line('PRODUCTION_COMPANY_ROLE DROPPED');
-    ELSE
-        dbms_output.put_line('PRODUCTION_COMPANY_ROLE DOES NOT EXIST');
-    END IF;
-
-    -- Check if APP_USER_ROLE exists before attempting to drop
-    SELECT
-        COUNT(1)
-    INTO ncount
-    FROM
-        dba_roles
-    WHERE
-        role = 'APP_USER_ROLE';
-
-    IF ( ncount > 0 ) THEN
-        -- Drop APP_USER_ROLE if it exists
-        EXECUTE IMMEDIATE 'DROP ROLE APP_USER_ROLE';
-        dbms_output.put_line('APP_USER_ROLE DROPPED');
-    ELSE
-        dbms_output.put_line('APP_USER_ROLE DOES NOT EXIST');
-    END IF;
-
     -- Check if production_company_user exists before attempting to drop
     SELECT
         COUNT(1)
@@ -76,81 +42,25 @@ END;
 DECLARE
     ncount NUMBER;
 BEGIN
-    -- Check if PRODUCTION_COMPANY_ROLE already exists
+    -- Check if PRODUCTION_COMPANY_USER already exists
     SELECT COUNT(1)
     INTO ncount
-    FROM dba_roles
-    WHERE role = 'PRODUCTION_COMPANY_ROLE';
+    FROM dba_users
+    WHERE username = 'PRODUCTION_COMPANY_USER';
 
     IF (ncount > 0) THEN
-        dbms_output.put_line('PRODUCTION_COMPANY_ROLE ALREADY EXISTS');
+        dbms_output.put_line('PRODUCTION_COMPANY_USER ALREADY EXISTS');
     ELSE
         -- Create PRODUCTION_COMPANY_ROLE
-        EXECUTE IMMEDIATE 'CREATE ROLE PRODUCTION_COMPANY_ROLE';
+        EXECUTE IMMEDIATE 'CREATE USER PRODUCTION_COMPANY_USER';
 
         -- Grant CONNECT and CREATE SESSION privileges
-        EXECUTE IMMEDIATE 'GRANT CREATE SESSION, CONNECT TO PRODUCTION_COMPANY_ROLE';
+        EXECUTE IMMEDIATE 'GRANT CREATE SESSION, CONNECT TO PRODUCTION_COMPANY_USER';
 
-        -- Check and grant privileges on tables
-        FOR tname IN (SELECT table_name FROM user_tables WHERE table_name IN ('TV_SHOW', 'MOVIE')) LOOP
-            EXECUTE IMMEDIATE 'GRANT SELECT, UPDATE, INSERT, DELETE ON ' || tname.table_name || ' TO PRODUCTION_COMPANY_ROLE';
-        END LOOP;
-
-        -- Check and grant privileges on views
-        FOR vname IN (SELECT view_name FROM user_views WHERE view_name IN ('MOVIE_GENRES', 'TV_SHOW_EPISODES', 'PRODUCTION_COMPANY_MOVIES', 'PRODUCTION_COMPANY_TV_SHOWS', 'GENRE_MEDIA_VIEW', 'ALL_MEDIA', 'NON_EXPLICIT_CONTENT_VIEW', 'EXPLICIT_CONTENT_VIEW')) LOOP
-            EXECUTE IMMEDIATE 'GRANT SELECT ON ' || vname.view_name || ' TO PRODUCTION_COMPANY_ROLE';
-        END LOOP;
+         -- Grant EXECUTE privilege on insert_into_production_co procedure
+        EXECUTE IMMEDIATE 'GRANT EXECUTE ON insert_into_production_co TO PRODUCTION_COMPANY_USER';
 
         COMMIT;
         dbms_output.put_line('PRODUCTION_COMPANY_ROLE is created and granted privileges');
     END IF;
 END;
-/
-
--- Create APP_USER_ROLE
-DECLARE
-    ncount NUMBER;
-BEGIN
-    -- Check if APP_USER_ROLE already exists
-    SELECT
-        COUNT(1)
-    INTO ncount
-    FROM
-        dba_roles
-    WHERE
-        role = 'APP_USER_ROLE';
-
-    IF ( ncount > 0 ) THEN
-        dbms_output.put_line('APP_USER_ROLE ALREADY EXISTS');
-    ELSE
-        -- Create APP_USER_ROLE
-        EXECUTE IMMEDIATE 'CREATE ROLE APP_USER_ROLE';
-        
-        -- Grant CONNECT and CREATE SESSION privileges
-        EXECUTE IMMEDIATE 'GRANT CREATE SESSION, CONNECT TO APP_USER_ROLE';
-        
-        -- Grant SELECT, UPDATE, INSERT, DELETE on specific tables
-        FOR x IN (
-            SELECT
-                *
-            FROM
-                user_tables
-            WHERE
-                table_name IN ( 'APP_USER', 'USER_PREFERENCES', 'FAVORITE_CONTENT' )
-        ) LOOP
-            EXECUTE IMMEDIATE 'GRANT SELECT, UPDATE, INSERT, DELETE ON '
-                              || x.table_name
-                              || ' TO APP_USER_ROLE';
-        END LOOP;
-
-        -- Grant SELECT on user-related views
-        EXECUTE IMMEDIATE 'GRANT SELECT ON APP_USER_VIEW TO APP_USER_ROLE';
-        EXECUTE IMMEDIATE 'GRANT SELECT ON USER_PREFERENCE_VIEW TO APP_USER_ROLE';
-        EXECUTE IMMEDIATE 'GRANT SELECT ON USER_FAVORITE_CONTENT TO APP_USER_ROLE';
-        EXECUTE IMMEDIATE 'GRANT SELECT ON ALL_MEDIA TO APP_USER_ROLE';
-        COMMIT;
-        dbms_output.put_line('APP_USER_ROLE is created and granted privileges');
-    END IF;
-
-END;
-/
